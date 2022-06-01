556f3d392b4fdd882a1d11ee478489115dee7cf1
Carolina Silva <65463404+Carolina-Silva@users.noreply.github.com>
2022-06-01 08:07:06 -0300

    sql sem inserts

--- AtividadeSpotify/spotify.sql
+++ AtividadeSpotify/spotify.sql
@@ -1,6 +1,13 @@
 CREATE DATABASE Spotify;
 use Spotify;
 
+CREATE TABLE TipoConta(
+	cd_TipoConta   	int 			not 	null 	auto_increment,
+	nm_TipoConta   	varchar(20) 	not 	null,
+    ds_TipoConta  	varchar(30) 	not 	null,
+    CONSTRAINT pk_TipoConta PRIMARY KEY (cd_TipoConta)
+);
+
 CREATE TABLE Usuario(
 	cd_Usuario   		int 			not 	null 	auto_increment,
 	nm_Usuario    		varchar(20) 	not 	null,
@@ -12,38 +19,29 @@ CREATE TABLE Usuario(
     CONSTRAINT fk_TipoConta FOREIGN KEY (cd_TipoConta)
             REFERENCES TipoConta(cd_TipoConta)
 );
-
-INSERT INTO Usuario (cd_Usuario, nm_Usuario, email_Usuario,senha_Usuario, img_Usuario)
-VALUES ();
-
-CREATE TABLE TipoConta(
-	cd_TipoConta   	int 			not 	null 	auto_increment,
-	nm_TipoConta   	varchar(20) 	not 	null,
-    ds_TipoConta  	varchar(30) 	not 	null,
-    CONSTRAINT pk_TipoConta PRIMARY KEY (cd_TipoConta)
+CREATE TABLE Genero(
+	cd_Genero   	int 		not 	null	auto_increment,
+    ds_Genero   	varchar(60) not 	null,
+    abr_Genero 		varchar(5) 	not 	null,
+    CONSTRAINT pk_Genero PRIMARY KEY (cd_Genero)
 );
-
-CREATE TABLE PlayList(
-	cd_PlayList   		int 			not 	null	auto_increment,
-	nm_PlayList    		varchar(20) 	not 	null,
-    dt_PlayList 		date 			not 	null,
-	cd_Usuario			int 			not 	null,
-    CONSTRAINT pk_PlayList   PRIMARY KEY (cd_PlayList),
-    CONSTRAINT fk_Usuario FOREIGN KEY (cd_Usuario)
-            REFERENCES Usuario(cd_Usuario)
+CREATE TABLE Artista(
+	cd_Artista   	int 			not 	null	auto_increment,
+	nm_Artista    	varchar(20) 	not 	null,
+    ds_Artista   	varchar(30) 	not 	null,
+    img_Artista 	mediumblob		not		null,
+    CONSTRAINT pk_Artista PRIMARY KEY (cd_Artista)
 );
-
-CREATE TABLE MusicaPlayList(
-	cd_Musica   			int 	not 	null,
-    cd_PlayList   			int 	not 	null,
-	qt_MusicaPlayList       int		not		null,
-    CONSTRAINT pk_MusicaPlayList   PRIMARY KEY (cd_Musica,cd_PlayList),
-    CONSTRAINT fk_Musica FOREIGN KEY (cd_Musica)
-            REFERENCES Musica (cd_Musica),
-	CONSTRAINT fk_PlayList FOREIGN KEY (cd_PlayList)
-            REFERENCES PlayList (cd_PlayList)
+CREATE TABLE Album(
+	cd_Album   			int 			not 	null	auto_increment,
+	nm_Album    		varchar(20) 	not 	null,
+    dt_LancamentoAlbum 	date 			not 	null,
+    img_Album	 		mediumblob		not		null,
+    cd_Artista			int 			not 	null,
+    CONSTRAINT pk_Album   PRIMARY KEY (cd_Album),
+    CONSTRAINT fk_Artista FOREIGN KEY (cd_Artista)
+            REFERENCES Artista(cd_Artista)
 );
-
 CREATE TABLE Musica(
 	cd_Musica   		int 			not 	null	auto_increment,
 	nm_Musica    		varchar(20) 	not 	null,
@@ -57,28 +55,22 @@ CREATE TABLE Musica(
             REFERENCES Album (cd_Album)
 );
 
-CREATE TABLE Genero(
-	cd_Genero   	int 		not 	null	auto_increment,
-    ds_Genero   	varchar(60) not 	null,
-    abr_Genero 		varchar(5) 	not 	null,
-    CONSTRAINT pk_Genero PRIMARY KEY (cd_Genero)
+CREATE TABLE PlayList(
+	cd_PlayList   		int 			not 	null	auto_increment,
+	nm_PlayList    		varchar(20) 	not 	null,
+    dt_PlayList 		date 			not 	null,
+	cd_Usuario			int 			not 	null,
+    CONSTRAINT pk_PlayList   PRIMARY KEY (cd_PlayList),
+    CONSTRAINT fk_Usuario FOREIGN KEY (cd_Usuario)
+            REFERENCES Usuario(cd_Usuario)
 );
-
-CREATE TABLE Album(
-	cd_Album   			int 			not 	null	auto_increment,
-	nm_Album    		varchar(20) 	not 	null,
-    dt_LancamentoAlbum 	date 			not 	null,
-    img_Album	 		mediumblob		not		null,
-    cd_Artista			int 			not 	null,
-    CONSTRAINT pk_Album   PRIMARY KEY (cd_Album),
-    CONSTRAINT fk_Artista FOREIGN KEY (cd_Artista)
-            REFERENCES Artista(cd_Artista)
-);
-
-CREATE TABLE Artista(
-	cd_Artista   	int 			not 	null	auto_increment,
-	nm_Artista    	varchar(20) 	not 	null,
-    ds_Artista   	varchar(30) 	not 	null,
-    img_Artista 	mediumblob		not		null,
-    CONSTRAINT pk_Artista PRIMARY KEY (cd_Artista)
+CREATE TABLE MusicaPlayList(
+	cd_Musica   			int 	not 	null,
+    cd_PlayList   			int 	not 	null,
+	qt_MusicaPlayList       int		not		null,
+    CONSTRAINT pk_MusicaPlayList   PRIMARY KEY (cd_Musica,cd_PlayList),
+    CONSTRAINT fk_Musica FOREIGN KEY (cd_Musica)
+            REFERENCES Musica (cd_Musica),
+	CONSTRAINT fk_PlayList FOREIGN KEY (cd_PlayList)
+            REFERENCES PlayList (cd_PlayList)
 );
