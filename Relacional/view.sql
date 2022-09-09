7b42a16709ba3220ca5d8185c89913fa4ee2614a
Carolina-Silva <carol_paiva@outlook.com>
2022-09-09 09:12:02 -0300

    add view

---
+++ Relacional/view.sql
@@ -0,0 +1,19 @@
+-- Testado em SQL server ----------------------------------------------
+
+CREATE TABLE departamento(
+    idDepto			integer 			not null 	PRIMARY KEY,
+    NomeDept 		varchar(15) 		not null,
+    Gerente 		integer 			not null,
+    Divisao 		varchar(10) 		not null,
+    local 			varchar(19) 		not null
+);
+
+insert into departamento ( idDepto,  NomeDept,  Gerente,  Divisao, local )
+				VALUES (100, 'vendas', 10, 'sul', 'diadema') ;
+                
+CREATE VIEW vwDepGerente as 
+SELECT nomedept, gerente FROM departamento;
+
+SELECT * from departamento;
+
+SELECT * from vwDepGerente;
